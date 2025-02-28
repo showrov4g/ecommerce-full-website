ce9a152bd67c422136e6a9e7e6c90a9bcf433f5c
showrov4g <showrovg00@gmail.com>
2025-02-28 22:51:59 +0600

    Fotter section design

---
+++ ecommerce-project/src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+
+const Footer = () => {
+    return (
+        <div className="bg-black">
+            <div className="py-10 w-11/12 mx-auto">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
--- ecommerce-project/src/layout/MainLayout.jsx
+++ ecommerce-project/src/layout/MainLayout.jsx
@@ -1,3 +1,4 @@
+import Footer from "../components/Footer/Footer";
 import Navbar from "../components/Navbar/Navbar";
 
 const MainLayout = () => {
@@ -5,6 +6,8 @@ const MainLayout = () => {
         <div>
             <Navbar></Navbar>
             <h1>Mian lay ouy</h1>
+            <div></div>
+            <Footer></Footer>
         </div>
     );
 };

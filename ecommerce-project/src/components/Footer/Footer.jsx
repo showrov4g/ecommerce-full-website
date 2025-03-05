08a90db95e71db639902c3aeb3ab8c262e6f3061
showrov4g <showrovg00@gmail.com>
2025-03-05 20:38:05 +0600

    cart components making

--- ecommerce-project/src/components/Footer/Footer.jsx
+++ ecommerce-project/src/components/Footer/Footer.jsx
@@ -32,7 +32,7 @@ const Footer = () => (
                                         <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
                                     </g>
                                 </svg>
-                                <input type="email" placeholder="mail@site.com" required />
+                                <input type="email" className='bg-transparent text-green-600' placeholder="mail@site.com" required />
                             </label>
                             <div className="validator-hint hidden">
                                 Enter valid email address
